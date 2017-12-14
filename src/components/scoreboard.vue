e8f847fdc6b5db7fbd100c23caf9d7049c85503e
moszie248 <moszie248@hotmail.com>
2017-12-15 06:26:49 +0700

    sc
    scc

--- src/components/scoreboard.vue
+++ src/components/scoreboard.vue
@@ -1,54 +1,42 @@
 <template>
 
   <div class="Content ">
-   <p class="title"> 
-     DRAWSOMETHING
-   </p>
-   <p class="title">   
-     TOP 5 HIGH SCORE
-   </p>
-   
-   <!-- <div class="columns" v-for="n in 5">
-      <li v-for="1 in 10">    
-      <hr>
-      <div class="column">
-        <p class="heading">Rank</p>
-        <p class="title">{{ n }} </p>
-        <p class="header" id="name">
-        <img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/23168020_1495904673820107_3419867865395687176_n.jpg?oh=cc10c9fe768e8f419c6fb0f35f30039a&oe=5A8BE786">
-        <br> Goh Nanthawad Promsorn</p>
-      </div>
-      <div class="column" style="color : red">
-        อันนี้มันต้องอยู่ทางขวาาาาาสิว้ะ<br>
-        SCORE : ???
-      </div>    
-   </div>
-  </div> -->
+    <p class="title"> 
+      DRAWSOMETHING      
+    </p>
+    <p class="title">   
+      TOP 5 HIGH SCORE
+    </p>
+    <div class="columns" v-for="n in 5">
+        <div class="column">
+        </div>
+        <div class="column">
+          <hr>
+          <p class="heading">Rank</p>
+          <p class="title">{{ n }} </p>
+          <p class="header" id="name">
+          <img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/23168020_1495904673820107_3419867865395687176_n.jpg?oh=cc10c9fe768e8f419c6fb0f35f30039a&oe=5A8BE786">
+          <br> 
+            Goh Nanthawad Promsorn</p>
+        </div>
+        <div class="column">
+            <hr>
+            <br><br><br>
+              <p class="title"> SCORE : ??? </p>
+        </div>
+        <div class="column">
+        </div>
+    </div>
+    <div>
+      <router-link v-bind:to="'lobby'">
+      <button class="button is-primary" > GO TO LOBBY </button>
+      </router-link>
+      <br><br>
+    </div>
 
-  <div class="columns" v-for="n in 5">
-    <div class="column">
-
-    </div>
-    <div class="column">
-      <hr>
-       <p class="heading">Rank</p>
-        <p class="title">{{ n }} </p>
-        <p class="header" id="name">
-        <img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/23168020_1495904673820107_3419867865395687176_n.jpg?oh=cc10c9fe768e8f419c6fb0f35f30039a&oe=5A8BE786">
-        <br> Goh Nanthawad Promsorn</p>
-    </div>
-    <div class="column">
-      <hr>
-      <br><br><br>
-      อันนี้มันต้องอยู่ทางขวาาาาาสิว้ะ<br>
-        <p> SCORE : ??? </p>
-    </div>
-    <div class="column">
-  
-    </div>
   </div>
-</div>
-  </template>
+
+</template>
 
   <style>
     #name {
