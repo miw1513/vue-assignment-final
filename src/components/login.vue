1d01cf7a0a5899c035976dd933c965cb6fd76111
chonticha <5806021633010@fitm.kmutnb.ac.th>
2017-12-14 16:52:37 +0700

    a
    a

---
+++ src/components/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="hero is-warning is-medium">
+  <div class="hero-body">
+    <center><img src="<a href="target="_blank"<img border="0" src="http://image.free.in.th/v/2013/ie/171125123203.png" alt="images by free.in.th" width="450px" height="450px"/></center>
+    <center><img style="margin-top:-6px"src="https://www.seeklogo.net/wp-content/uploads/2016/09/facebook-icon-preview-1.png" width="50px" height="50px"><a class="button is-info">login with facebook</a>
+    </center>
+  </div>
+</section>
+
+</div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
