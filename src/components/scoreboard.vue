b5126d58b1b31d33d745eade000ca322117583fe
Nanthawad <32787546+Nanthawad@users.noreply.github.com>
2017-12-15 00:03:01 +0700

    gggg

---
+++ src/components/scoreboard.vue
@@ -0,0 +1,40 @@
+<template>
+<nav class="level">
+<div class="level-item has-text-centered">
+  <div>
+    <p class="heading">Rank</p>
+    <p class="title">1</p>
+    <hr>
+    <p class="title">2</p>
+  </div>
+</div>
+<div class="level-item has-text-centered">
+  <div>
+    <p class="heading">Player Name</p>
+    <p class="title" id="name"><img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/23168020_1495904673820107_3419867865395687176_n.jpg?oh=cc10c9fe768e8f419c6fb0f35f30039a&oe=5A8BE786"> Goh Nanthawad Promsorn</p>
+    <hr>
+    <p class="title" id="name"><img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/15781146_1247901371922442_1848703223085746596_n.jpg?oh=2daa5ed4638dcc5750de7f25af35b635&oe=5AC8AFCD"> Mos Sirinun</p>
+  </div>
+</div>
+<div class="level-item has-text-centered">
+  <div>
+    <p class="heading">Score</p>
+    <p class="title">120</p>
+    <hr>
+    <p class="title">100</p>
+  </div>
+</div>
+</div>
+</nav>
+  </template>
+
+  <style>
+    #name {
+      color: #7FFFD4;  
+    }
+    img {
+      border-radius: 100%;
+      width: 50px;
+      height: 50px
+    }
+    </style>
